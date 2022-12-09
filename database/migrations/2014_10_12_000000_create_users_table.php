f95b15f3bfbe683e9c9bbad2dad1538b532a5e33
Aradhana Indra Daniswara <aradhanaindradev@gmail.com>
2022-12-09 13:41:55 +0700

    Add User Migration, Change Global Font

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('image');
+            $table->boolean('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
