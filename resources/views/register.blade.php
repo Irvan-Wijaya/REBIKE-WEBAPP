b89b94c9f88aada75dfa2a0290f92cc9da26866a
Jasson Laurent <jassonlaurent29@gmail.com>
2022-12-12 22:31:12 +0700

    Update Register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -12,6 +12,10 @@
                     <h2 class="form-title">Sign up</h2>
                     <form method="POST" class="register-form" id="register-form">
                         <div class="form-group">
+                            <label for="username"><i class="zmdi zmdi-account material-icons-username"></i></label>
+                            <input type="text" name="username" id="username" placeholder="Your Username"/>
+                        </div>
+                        <div class="form-group">
                             <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
                             <input type="text" name="name" id="name" placeholder="Your Name"/>
                         </div>
@@ -27,17 +31,13 @@
                             <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
                             <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password"/>
                         </div>
-                        <div class="form-group">
-                            <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
-                            <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
-                        </div>
                         <div class="form-group form-button">
                             <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
                         </div>
                     </form>
                 </div>
                 <div class="signup-image">
-                    <figure><img src="images/signup-image.jpg" alt="sing up image"></figure>
+                    <figure><img src="images/signup-image.jpg" alt="sign up image"></figure>
                     <a href="#" class="signup-image-link">I am already member</a>
                 </div>
             </div>
