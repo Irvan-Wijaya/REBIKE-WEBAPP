7950045feded7e68943513d8e18e4e29ecaca586
marcolcu <116879840+marcolcuu@users.noreply.github.com>
2022-12-14 09:49:09 +0700

    marcoUpdate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,11 +20,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('image');
-            $table->boolean('is_admin');
+            $table->string('image')->nullable();
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
-        });
+        }); 
     }
 
     /**
