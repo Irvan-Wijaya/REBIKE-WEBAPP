c8308d4b981dac73a223d79d9ad82c2431c3f719
Aradhana Indra Daniswara <aradhanaindradev@gmail.com>
2022-12-15 09:52:13 +0700

    Add Forum Create Feature

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::get('/faq', function () {
 // Forum
 Route::get('/forum', [ForumController::class, 'index']);
 Route::get('/forum/create', [ForumController::class, 'create'])->middleware('auth');
+Route::post('/forum/create', [ForumController::class, 'store'])->middleware('auth');
 Route::get('/forum/post/{post:slug}', [ForumController::class, 'show']);
 
 Auth::routes();
