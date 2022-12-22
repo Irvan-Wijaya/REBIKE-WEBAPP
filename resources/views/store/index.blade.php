37a32fcba4c1f6c5b93a34959e49d95f03625c52
Jasson Laurent <jassonlaurent29@gmail.com>
2022-12-22 11:24:54 +0700

    jassonUpdate

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -167,5 +167,35 @@
             </div>
         </div>
 
+
     </div>
-@endsection
+    <div class="container mt-4 d-flex justify-content-between" >
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+      </div>
+    </div>
+    @endsection
