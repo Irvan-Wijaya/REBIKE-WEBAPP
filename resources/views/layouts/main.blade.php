649153fc3c97a9517970ca3bc2a65c16da5a82d8
Aradhana Indra Daniswara <aradhanaindradev@gmail.com>
2022-12-29 12:34:20 +0700

    Remove the usage of Vite from the app

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,12 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
     {{-- Trix Editor --}}
     <link rel="icon" type="image/x-icon" href="{{ url('Images/Favicon.png') }}">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
     <script type="text/javascript" src="https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js"></script>
-    @vite(['resources/sass/app.scss'])
+    {{-- @vite(['resources/sass/app.scss']) --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('extra')
     <title>REBIKE</title>
 </head>
